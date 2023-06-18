20049dcd71ad8b98a05ce52e20706c2b76d4a284
Clare Tory <93897391+claretory@users.noreply.github.com>
2023-06-18 21:51:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,4 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from web3 import Web3
-from auto_metamask import*
 import time
